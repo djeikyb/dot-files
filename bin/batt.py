048cb6108396dc4b634c02d1714c2eb7cc41f6ac
Jacob Certain <jace42+git@gmail.com>
2012-03-21 04:28:03 -0700

    much revision. aiming for a functional style

--- bin/batt.py
+++ bin/batt.py
@@ -4,38 +4,68 @@ import time
 import datetime
 from configobj import ConfigObj
 
-#LOG = /var/log/battery.log
-LOG = '/tmp/battery.log'
+# IN: battery information from /sys
+# OUT: datetime & power to log
+# OUT: power info to file for tmux to scrape
+
+# FUTURE FEATURE
+# - collect screen brightness
+# - collect wifi on/off
+# - collect wifi signal strength
+# - collect memory usage
+# - note when wake from hibernate
+# - graphical indicator
+
+# BUGS
+# none?
+
+def get_config():
+    uevent = '/sys/class/power_supply/BAT0/uevent'
+    return ConfigObj(uevent)
+
+def state(cfg):
+    state = cfg['POWER_SUPPLY_STATUS']
+    return state
+
+def charge(cfg):
+    charge_raw = float(cfg['POWER_SUPPLY_CHARGE_NOW'])
+    charge_full = float(cfg['POWER_SUPPLY_CHARGE_FULL'])
+    charge = (charge_raw / charge_full) * 100
+    return charge
+
+def volt(cfg):
+    volt_raw = float(cfg['POWER_SUPPLY_VOLTAGE_NOW'])
+    volt_min = float(cfg['POWER_SUPPLY_VOLTAGE_MIN_DESIGN'])
+    volt = (volt_raw / volt_min) * 100
+    return volt
+
+def out_log(cfg):
+    now = datetime.datetime.now().strftime("%F %H:%M:%S")
+    status = '%d;%.2f;%s;%s;%s;%s\n' \
+        % (charge(cfg), volt(cfg), now, cfg['POWER_SUPPLY_CHARGE_NOW'], \
+           cfg['POWER_SUPPLY_VOLTAGE_NOW'], state(cfg))
+    return status
+
+def out_tmux(cfg):
+    status = 'b%d v%d\n' % (charge(cfg), volt(cfg))
+    return status
 
 def wait():
     time.sleep(5)
-
-def get_status():
-    global status
-
-    FILE = '/sys/class/power_supply/BAT0/uevent'
-    config = ConfigObj(FILE)
-
-    state = config['POWER_SUPPLY_STATUS']
-    charge = float(config['POWER_SUPPLY_CHARGE_NOW'])
-    charge_full = float(config['POWER_SUPPLY_CHARGE_FULL'])
-    voltage = float(config['POWER_SUPPLY_VOLTAGE_NOW'])
-    voltage_min = float(config['POWER_SUPPLY_VOLTAGE_MIN_DESIGN'])
-
-    charge_percent = (charge / charge_full) * 100
-    voltage_percent = (voltage / voltage_min) * 100
-
-    now = datetime.datetime.now().strftime("%F %H:%M:%S")
-
-    status = '%d %.2f %s %d %d\n' \
-        % (charge_percent, voltage_percent, now, charge, voltage)
-
-    return state
+    return 0
 
 while True:
-    while get_status() != 'Full':
-        f = open(LOG, 'a')
-        f.write(status)
+    config = get_config()
+    while state(config) != 'Full':
+        # write long-term log
+        f = open("/home/jake/battery/charge.log", 'a')
+        f.write(out_log(config))
         f.close()
+        # overwrite tmux log
+        f = open('/tmp/tmux_batt', 'w')
+        f.write(out_tmux(config))
+        f.close()
+        # loop...
         wait()
+        config = get_config()
     wait()
